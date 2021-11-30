4d625553762022afbae08d8f3129051958f84164
Adrian Brown <adrian.r.brown90@gmail.com>
2021-11-30 14:30:48 -0700

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+//Exercise #1 Take your first "Test Drive" by writing your first test!
+function sayHello(name) {
+    if (name === 21){
+        return "that's not a name!"
+    } else if (name){
+        return "Hello " + name + "!";
+    } else {
+        return 'Hello!';
+    }
+}
+
--- tests.js
+++ tests.js
@@ -13,3 +13,26 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+//Exercise #1 Take your first "Test Drive" by writing your first test!
+
+describe('sayHello Unit Tests', function() {
+    it('should be a defined function', function () {
+        expect(sayHello).toBeDefined();
+    });
+    it('should return a string', function () {
+        expect(typeof helloWorld()).toBe('string')
+    });
+    it('should return "Hello!" when no arguments are passed', function () {
+        expect(sayHello()).toBe("Hello!")
+    });
+    it('should return "Hello Angel!" when called with argument "Angel".', function () {
+        expect(sayHello("Angel!")).toBe('Hello Angel!')
+    });
+    it('should return "Hello Andrew!" when called with argument "Andrew".', function () {
+        expect(sayHello("Andrew!")).toBe('Hello Andrew!')
+    });
+    it('That\'s not a name! when called with argument (number) 21.', function () {
+        expect(sayHello(21)).toBe('That\'s not a name!')
+    });
+});
