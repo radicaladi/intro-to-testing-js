4f289575c1728586da414dca016d2c1d6b83756f
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-01 09:09:44 -0700

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -5,13 +5,5 @@ function helloWorld() {
 
 //Exercise #1 Take your first "Test Drive" by writing your first test!
 function sayHello(name) {
-    if (name === 21){
-        return "that's not a name!"
-    } else if (name){
-        return "Hello " + name + "!";
-    } else {
-        return 'Hello!';
-    }
+    return "Hello, " + name + "!"
 }
-
-// Exercise #2 Ensure our function returns the right data type.
--- tests.js
+++ tests.js
@@ -23,19 +23,21 @@ describe('sayHello Unit Tests', function() {
     it('should return a string', function () {
         expect(typeof helloWorld()).toBe('string')
     });
-    it('should return "Hello!" when no arguments are passed', function () {
-        expect(sayHello()).toBe("Hello!")
+    it('should return "Hello, Jane!" when called with argument "Jane".', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!")
     });
-    it('should return "Hello Angel!" when called with argument "Angel".', function () {
-        expect(sayHello("Angel!")).toBe('Hello Angel!')
+    it('should return "Hello, Alex!" when called with "Alex".', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!")
     });
-    it('should return "Hello Andrew!" when called with argument "Andrew".', function () {
-        expect(sayHello("Andrew!")).toBe('Hello Andrew!')
-    });
-    it('That\'s not a name! when called with argument (number) 21.', function () {
-        expect(sayHello(21)).toBe('That\'s not a name!')
+    it('should return "Hello Pat!" when called with argument "Pat".', function () {
+        expect(sayHello("Pat")).toBe('Hello, Pat!')
     });
 });
 
-// Exercise #2 Ensure our function returns the right data type.
+// it('That\'s not a name! when called with argument (number) 21.', function () {
+//     expect(sayHello(21)).toBe('That\'s not a name!')
+// });
 
+//it('should return "Hello!" when no arguments are passed', function () {
+//         expect(sayHello()).toBe("Hello!")
+//     });
